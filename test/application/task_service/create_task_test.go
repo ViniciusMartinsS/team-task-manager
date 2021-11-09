b10204e050942ee6508d90af73458eb41bab59b3
Vinicius Martins <visimonemartins@hotmail.com>
2021-11-09 21:52:59 +0000

    master Add task create tests

---
+++ test/application/task_service/create_task_test.go
@@ -0,0 +1,51 @@
+package taskservice
+
+import (
+	"testing"
+
+	"github.com/ViniciusMartinsS/manager/internal/application/service"
+	constant "github.com/ViniciusMartinsS/manager/internal/common"
+	"github.com/ViniciusMartinsS/manager/internal/domain/model"
+	"github.com/ViniciusMartinsS/manager/test/mock/application"
+	"github.com/ViniciusMartinsS/manager/test/mock/infrastructure"
+	. "github.com/onsi/gomega"
+)
+
+func TestTaskServiceCreateMethod(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	encryptionService := application.NewEncryptionServiceMock("")
+	payload := model.TaskPayload{Name: "Example", Summary: "Task Example", Performed: "09/11/2021"}
+
+	t.Run("Expect list task successfully to the technician role", func(it *testing.T) {
+		var resultType []model.TaskResponseContent
+
+		userRepository := infrastructure.NewuserRepositoryMock(false, false)
+		taskRepository := infrastructure.NewTaskRepositoryMock(false, false)
+		notificationService := application.NewNotificationServiceMock(userRepository)
+
+		task := service.
+			NewTaskService(taskRepository, userRepository, notificationService, encryptionService).
+			Create(TECHNICIAN_ID, payload)
+
+		g.Expect(task.Code).To(Equal(constant.SUCCESS_CODE))
+		g.Expect(task.Result).To(BeAssignableToTypeOf(resultType))
+		g.Expect(task.Message).To(BeEmpty())
+	})
+
+	t.Run("Expect internal server error", func(it *testing.T) {
+		var resultType []model.TaskResponseContent
+
+		userRepository := infrastructure.NewuserRepositoryMock(false, false)
+		taskRepository := infrastructure.NewTaskRepositoryMock(true, false)
+		notificationService := application.NewNotificationServiceMock(userRepository)
+
+		task := service.
+			NewTaskService(taskRepository, userRepository, notificationService, encryptionService).
+			Create(TECHNICIAN_ID, payload)
+
+		g.Expect(task.Result).To(Equal(resultType))
+		g.Expect(task.Code).To(Equal(constant.INTERNAL_SERVER_ERROR_CODE))
+		g.Expect(task.Message).To(Equal(constant.INTERNAL_SERVER_ERROR_MESSAGE))
+	})
+}
