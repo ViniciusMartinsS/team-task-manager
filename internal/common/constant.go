689a72129f756eb77de3a4811821cf373b53b035
Vinicius Martins <31781665+ViniciusMartinsS@users.noreply.github.com>
2021-11-10 00:10:24 +0000

    Update constant.go

--- internal/common/constant.go
+++ internal/common/constant.go
@@ -14,7 +14,6 @@ const (
 	FORBIDDEN_ERROR_MESSAGE        = "Hmmmm... It seems you are not allowed to do such a thing. Ask for your manager help!"
 	NOT_AUTHORIZED_ERROR_MESSAGE   = "Stop right there! You are unauthorized!"
 	DATE_BAD_REQUEST               = "key: 'TaskCreate.Summary' Error:Field validation for 'Performed' failed on the 'format' regex"
-	TYPE_BAD_REQUEST               = ""
 	RECORD_NOT_FOUND_LIST_MESSAGE  = "You do not have any tasks. Create a new one & let's get to work! ;)"
 
 	DATE_REGEX          = `^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$`
